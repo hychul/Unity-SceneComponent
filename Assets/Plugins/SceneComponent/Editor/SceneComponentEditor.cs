52ef9a4baf37477e2342e62d80d575288e3d5fc7
hychul <hychome@gmail.com>
2018-07-03 23:57:42 +0900

    Initial commit

---
+++ Assets/Plugins/SceneComponent/Editor/SceneComponentEditor.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace SceneComponent
+{
+    [CustomEditor(typeof(SceneAsset))]
+    public class SceneComponentEditor : Editor
+    {
+        private GameObject scenePrefab;
+
+        Dictionary<Editor, bool> activeEditors = new Dictionary<Editor, bool>();
+
+        void OnEnable()
+        {
+            Undo.undoRedoPerformed += InitActiveEditors;
+
+            var assetPath = AssetDatabase.GetAssetPath(target);
+
+            scenePrefab = SceneEntity.GetScenePrefab(assetPath);
+
+            if (scenePrefab == null)
+                scenePrefab = SceneEntity.CreateScenePrefab(assetPath);
+
+            InitActiveEditors();
+        }
+
+        void OnDisable()
+        {
+            Undo.undoRedoPerformed -= InitActiveEditors;
+
+            ClearActiveEditors();
+
+            AssetDatabase.SaveAssets();
+        }
+
+        void InitActiveEditors()
+        {
+            ClearActiveEditors();
+
+            foreach (var component in scenePrefab.GetComponents<Component>())
+            {
+                if (component is Transform)
+                    continue;
+
+                activeEditors.Add(CreateEditor(component), true);
+            }
+        }
+
+        void ClearActiveEditors()
+        {
+            foreach (var activeEditor in activeEditors)
+                DestroyImmediate(activeEditor.Key);
+
+            activeEditors.Clear();
+        }
+
+        public override void OnInspectorGUI()
+        {
+            GUI.enabled = true;
+
+            var editors = new List<Editor>(activeEditors.Keys);
+
+            DrawInspector(editors);
+
+            Interact(editors);
+        }
+
+        void DrawInspector(List<Editor> editors)
+        {
+            foreach (var editor in editors)
+                DrawEditor(editor);
+        }
+
+        void DrawEditor(Editor editor)
+        {
+            DrawEditorTitle(editor);
+            DrawEditorBody(editor);
+        }
+
+        void DrawEditorTitle(Editor editor)
+        {
+            var rect = GUILayoutUtility.GetRect(GUIContent.none, GUIStyle.none, GUILayout.Height(20));
+            rect.x = 0;
+            rect.y -= 5;
+            rect.width += 20;
+
+            activeEditors[editor] = EditorGUI.InspectorTitlebar(rect, activeEditors[editor], editor.target, true);
+
+            GUILayout.Space(-5f);
+        }
+
+        void DrawEditorBody(Editor editor)
+        {
+            if (activeEditors[editor] && editor.target != null)
+                editor.OnInspectorGUI();
+
+            EditorGUILayout.Space();
+        }
+
+        void Interact(List<Editor> editors)
+        {
+            if (editors.Any(e => e.target == null))
+            {
+                InitActiveEditors();
+                Repaint();
+            }
+
+            var dragAndDropRect = GUILayoutUtility.GetRect(GUIContent.none,
+                                                           GUIStyle.none,
+                                                           GUILayout.ExpandHeight(true),
+                                                           GUILayout.MinHeight(200));
+
+            if (!dragAndDropRect.Contains(Event.current.mousePosition))
+                return;
+
+            switch (Event.current.type)
+            {
+                case EventType.DragUpdated:
+                case EventType.DragPerform:
+                    DragAndDrop.visualMode = DragAndDropVisualMode.Copy;
+
+                    if (Event.current.type == EventType.DragPerform)
+                    {
+                        DragAndDrop.AcceptDrag();
+
+                        var components = DragAndDrop.objectReferences
+                                                    .Where(x => x is MonoScript)
+                                                    .OfType<MonoScript>()
+                                                    .Select(m => m.GetClass());
+
+                        foreach (var component in components)
+                            Undo.AddComponent(scenePrefab, component);
+
+                        InitActiveEditors();
+                    }
+
+                    break;
+            }
+        }
+    }
+}
---
+++ Assets/Plugins/SceneComponent/Scripts/SceneEntity.cs
@@ -0,0 +1,84 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace SceneComponent
+{
+    public class SceneEntity
+    {
+        private const string PREFAB_PATH = "Assets/Plugins/SceneComponent/Prefabs";
+        private const string PREFAB_FORMAT = "{0}/{1}.prefab";
+        private const string COMPONENT_CONTAINER = "SceneComponentContainer";
+
+        private static GameObject container;
+
+        [InitializeOnLoadMethod]
+        static void CreatePrefabFolder()
+        {
+            Directory.CreateDirectory(PREFAB_PATH);
+        }
+
+#if UNITY_EDITOR
+        public static GameObject CreateScenePrefab(string scenePath, params System.Type[] components)
+        {
+            var guid = AssetDatabase.AssetPathToGUID(scenePath);
+            var prefabPath = string.Format(PREFAB_FORMAT, PREFAB_PATH, guid);
+            
+            var go = new GameObject(guid, components);
+            
+            var prefab = PrefabUtility.CreatePrefab(prefabPath, go);
+            
+            Object.DestroyImmediate(go);
+            
+            return prefab;
+        }
+#endif
+
+        public static GameObject GetScenePrefab(string scenePath)
+        {
+            var guid = AssetDatabase.AssetPathToGUID(scenePath);
+            var prefabPath = string.Format(PREFAB_FORMAT, PREFAB_PATH, guid);
+            
+            return AssetDatabase.LoadAssetAtPath<GameObject>(prefabPath);
+        }
+
+        [PostProcessScene]
+        static void OnPostProcessScene()
+        {
+            var scenePath = SceneManager.GetActiveScene().path;
+
+            if (string.IsNullOrEmpty(scenePath))
+                return;
+
+            var prefab = GetScenePrefab(scenePath);
+
+            if (prefab)
+            {
+                container = Object.Instantiate(prefab);
+                container.name = COMPONENT_CONTAINER;
+            }
+            else
+            {
+                container = null;
+            }
+        }
+
+        public static T[] GetComponents<T>()
+        {
+            if (container == null)
+                return null;
+
+            return container.GetComponents<T>();
+        }
+
+        public static T GetComponent<T>()
+        {
+            if (container == null)
+                return default(T);
+
+            return container.GetComponent<T>();
+        }
+    }
+}
